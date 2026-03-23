07d4b6d1df34e922612db5bfee8e1d141cf0665c
Tal <talrhv1@gmail.com>
2026-03-23 20:42:34 +0200

    try to make matter work

--- util/datautil.mjs
+++ util/datautil.mjs
@@ -1,18 +1,35 @@
+"use strict";
+
 class DataUtil {
   constructor() {}
 
   getSubService(status) {
-    var subTypeArr = [];
-    for (var map of status) {
-      if (map.code.indexOf("switch") != -1) {
+    if (!Array.isArray(status)) return { subType: [] };
+
+    const subTypeArr = [];
+
+    for (const map of status) {
+      if (map.code.toLowerCase().includes("switch")) {
         if (typeof map.value === "boolean") {
           subTypeArr.push(map.code);
         }
       }
     }
+
+    subTypeArr.sort((a, b) => {
+      const numA = parseInt(a.replace(/\D/g, "")) || 0;
+      const numB = parseInt(b.replace(/\D/g, "")) || 0;
+      return numA - numB;
+    });
+
     return {
       subType: subTypeArr,
     };
+  }
+
+  getFriendlyName(code) {
+    const num = code.match(/\d+$/);
+    return num ? `Switch ${num[0]}` : "Switch";
   }
 }
 
